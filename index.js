54d3fc50e5d49362f84d06215a90f35f86b8e6af
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-10-22 13:45:57 -0700

    Convert module from CJS to ESM

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const http = require("http");
-const fs = require("fs");
-const { error } = require("console");
+import http from "http";
+import fs from "fs/promises";
+import path from "path";
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const pathUrl = {
-  "/": "./routes/index.html",
-  "/about": "./routes/about.html",
-  "/contact-me": "./routes/contact-me.html",
-  "/404": "./routes/404.html",
+  "/": path.join(__dirname, "routes", "index.html"),
+  "/about": path.join(__dirname, "routes", "about.html"),
+  "/contact-me": path.join(__dirname, "routes", "contact-me.html"),
+  "/404": path.join(__dirname, "routes", "404.html"),
 };
-const server = http.createServer((req, res) => {
-  const filePath = pathUrl[req.url] || "./routes/404.html";
 
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(500, { "content-type": "text/html" });
-      return res.end("Internal Server Error");
-    }
+const server = http.createServer(async (req, res) => {
+  const filePath = pathUrl[req.url] || pathUrl["/404"];
+  const statusCode = filePath.includes("404") ? 404 : 200;
 
-    const statusCode = filePath.includes("404") ? 404 : 200;
+  try {
+    const data = await fs.readFile(filePath);
     res.writeHead(statusCode, { "content-type": "text/html" });
     res.end(data);
-  });
+  } catch (err) {
+    res.writeHead(500, { "content-type": "text/html" });
+    res.end("Internal Server Error");
+  }
 });
 
 server.listen(8080);
