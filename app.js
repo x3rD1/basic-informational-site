02f44b49e3637c6303db25d86345984a07e83e2e
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-10-23 13:11:00 -0700

    Installed Express and rewrite code into Express

---
+++ app.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import path from "path";
+import url from "url";
+import express from "express";
+
+const app = express();
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(
+  express.static(path.join(__dirname, "routes"), { extensions: ["html"] })
+);
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "routes", "404.html"));
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send("Internal Server Error");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT || 3000, (error) => {
+  if (error) {
+    throw error;
+  }
+});
